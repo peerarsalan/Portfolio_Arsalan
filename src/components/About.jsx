47ef15b075343a93b1bbfcf5caaf9fec22ca48cf
peerarsalan <peerarsalan7@gmail.com>
2024-01-18 11:49:32 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const About = () => { 
+  return (
+    <div
+      name="about"
+      id="about"
+      //#0a192f old bg color
+      className="w-full h-screen bg-[black] text-black"
+    >
+      <div className="flex flex-col justify-center items-center w-full lg:h-full">
+        <div className=" rounded-md bg-gradient-to-r from-blue-500 to-cyan-500 flex flex-col justify-center items-center w-4/6 ">
+          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
+            <div className="sm:text-right pb-8 pl-4">
+              <p className="text-4xl  font-bold inline border-b-4 text-white border-cyan-500 ">
+                About
+              </p>
+            </div>
+            <div></div>
+          </div>
+          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+            <div className="sm:text-right text-white stext-3xl font-bold sm:text-3xl">
+              <p>
+                Hi. I'm PEER ARSALAN AHMAD, nice to meet you. Do take a
+                look around.
+              </p>
+            </div>
+            <div>
+              <p>
+                {" "}
+                A software developer with experience in building Responsive and
+                Scalable Web apps. I am well-knowledged in UI/UX principles and
+                practices. In addition to software development, I am also a
+                technical writer--simplifying topics/concepts on the web.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import me from '../assets/main.png';
+import { Link } from "react-scroll"; 
+
+const Home = () => {
+  return (
+    <div
+    name="home"
+    className="h-full w-full bg-[black] md:h-full"
+  >
+    <div className="max-w-screen-lg mx-auto pt-20 flex flex-col items-center justify-center h-full px-4  md:flex-row ">
+      <div className="flex flex-col justify-center h-full sm:pt-90">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white ">
+          Hi there! I'm a Front-End Web Developer
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+           I have 4 years of experience in web design and development.
+          Currently, I love to work on web application using technologies like
+          React JS, Tailwind, CMS and other Frontend Technologies.
+        </p>
+        <div>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <img
+          src={me}  
+          alt="my profile"
+          className="rounded-2xl mx-auto w-2/3 md:w-full"
+        />
+      </div>
+    </div>
+  </div>
+  );
+};
+export default Home;
